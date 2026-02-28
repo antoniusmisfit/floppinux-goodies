568d7f35f0dad02f0cbd1643b4795b015def73f0
Tony Agudo <antoniusmisfit@gmail.com>
2026-02-28 02:46:42 -0500

    Update shutdown.sh with trap instruction
    Added a note to execute the shutdown script with trap for proper shutdown.

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This is a rudimentary "shutdown" script for Floppinux
+# Please execute "trap /path/to/shutdown.sh EXIT" at the shell to ensure proper shutdown
 # Sync before shutdown
 sync
 echo "Shutdown complete. You may turn off the machine now."
